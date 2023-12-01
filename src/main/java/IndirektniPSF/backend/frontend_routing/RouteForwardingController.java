1806245cb04ed92c7de16dc49720aad73a430b8e
Simo Mrdjen <dr.sizni@gmail.com>
2023-12-01 13:09:29 +0100

    added RFController

---
+++ src/main/java/IndirektniPSF/backend/frontend_routing/RouteForwardingController.java
@@ -0,0 +1,15 @@
+package IndirektniPSF.backend.frontend_routing;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RouteForwardingController {
+
+    @RequestMapping(value = "/**/{path:[^\\.]*}")
+    public String forward() {
+        return "forward:/";
+    }
+}
+
+
